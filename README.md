0bb8bbb2c93a7d467973f477c5db3ed2a7308de9
Sam Estep <sam@samestep.com>
2025-04-02 13:11:49 -0400

    Add a first version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# vscode-save-constantly
-VS Code toggle to save on every keystroke.
+# Save Constantly (VS Code)
+
+This is a VS Code extension that lets you toggle documents to save (without formatting) on every change.
+
+## Usage
+
+After installing, execute the command **Save Constantly: Toggle** (bound to **`alt+s`** by default) to enable for the document associated with the active editor. Repeat to disable.
+
+## Issues
+
+If a change occurs in a file with Save Constantly enabled but not currently associated with the active editor, it will not be saved; see [this Stack Overflow question](https://stackoverflow.com/q/79550924/5044950) for more details.
+
+## Related
+
+Michel Betancourt's [Save Typing](https://marketplace.visualstudio.com/items?itemName=akhail.save-typing) extension is similar, but has a couple drawbacks:
+
+- It autoformats on save, making it unusable with the **Files: Trim Trailing Whitespace** setting enabled.
+- It's configured per language, making it inconvenient to toggle.
+- It's not open source.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
---
+++ src/extension.ts
@@ -0,0 +1,66 @@
+import * as vscode from "vscode";
+
+export const activate = (context: vscode.ExtensionContext) => {
+  const log = vscode.window.createOutputChannel("Save Constantly", {
+    log: true,
+  });
+  log.info("activating Save Constantly extension");
+
+  const save = async (document: vscode.TextDocument) => {
+    const { fileName } = document;
+    log.trace("saving", fileName);
+    const active = vscode.window.activeTextEditor?.document.fileName;
+    if (fileName !== active) {
+      // https://stackoverflow.com/q/79550924/5044950
+      log.warn("can't save a file different from the active document", active);
+      return;
+    }
+    await vscode.commands.executeCommand(
+      "workbench.action.files.saveWithoutFormatting"
+    );
+  };
+
+  const files = new Set<string>();
+
+  const statusBarItem = vscode.window.createStatusBarItem();
+  statusBarItem.text = "save constantly";
+  statusBarItem.tooltip = "Saving this file on every change. Click to disable.";
+  statusBarItem.command = "saveConstantly.toggle";
+  const updateStatusBar = () => {
+    const file = vscode.window.activeTextEditor?.document.fileName;
+    if (file !== undefined && files.has(file)) statusBarItem.show();
+    else statusBarItem.hide();
+  };
+  updateStatusBar();
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("saveConstantly.toggle", async () => {
+      const document = vscode.window.activeTextEditor?.document;
+      if (document === undefined) {
+        vscode.window.showErrorMessage("no active editor");
+        return;
+      }
+      const { fileName } = document;
+      if (files.has(fileName)) {
+        log.debug("toggle off", fileName);
+        files.delete(fileName);
+        updateStatusBar();
+      } else {
+        log.debug("toggle on", fileName);
+        files.add(fileName);
+        updateStatusBar();
+        await save(document);
+      }
+    }),
+
+    vscode.workspace.onDidChangeTextDocument(async (event) => {
+      const { document } = event;
+      const { fileName, isDirty } = document;
+      if (!(files.has(fileName) && isDirty)) return;
+      await save(document);
+    }),
+
+    statusBarItem,
+    vscode.window.onDidChangeActiveTextEditor(updateStatusBar)
+  );
+};
